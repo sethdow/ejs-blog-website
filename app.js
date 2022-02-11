3453c7cd66fc765fd0065dceb849a93b470a189a
sethdow <seth.dow@gmail.com>
2022-02-10 21:43:34 -0800

    Added form for posting blogs, added array for accumulating blogs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = []
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +18,7 @@ app.use(express.static("public"));
 
 
 app.get('/home', function(req,res) {
-  res.render('home',{homeStartingContent})
+  res.render('home',{homeStartingContent, posts})
 })
 
 app.get('/', function(req,res) {
@@ -32,6 +33,20 @@ app.get('/contact', function(req,res) {
   res.render('contact',{contactContent})
 })
 
+
+app.get('/compose', function(req,res) {
+  res.render('compose')
+})
+
+app.post('/compose', function(req,res) {
+  let post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  }
+  posts.push(post)
+
+  res.redirect('/')
+})
 
 
 
